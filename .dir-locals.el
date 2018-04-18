1d4489ecbb7e6ae6006f5b7444a229e0c3bdf1b6
Vishakh Kumar <grokkingStuff@gmail.com>
2018-04-18 02:00:50 -0400

    Created custom TODO types for ease of reference and issue tracking.

---
+++ .dir-locals.el
@@ -0,0 +1,21 @@
+(
+  (org . (
+          ;; Set custom org-mode headline tags
+          (setq org-todo-keywords '(
+                                    (type  "Vi-TODO" 
+                                           "Dharsh-TODO" 
+                                           "ASK-Trey" 
+                                           
+                                           "Vi-CURRENT(!)" 
+                                           "Dharsh-CURRENT(!)" 
+          
+                                           "|" 
+          
+                                           "Vi-Owner(!)"
+                                           "Dharsh-Owner(!)")
+                                   )
+          )
+          
+         )
+  )
+)
